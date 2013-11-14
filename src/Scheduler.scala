bcb342f18759317768f463dc8e2133cd3e2ee463
headinthebox <emeijer@applied-duality.com>
2013-11-13 19:54:31 -0800

    Add files

---
+++ src/Scheduler.scala
@@ -0,0 +1,203 @@
+package rx.lang.scala
+
+import java.util.Date
+
+import scala.concurrent.duration.Duration
+import scala.language.postfixOps
+import ImplicitFunctionConversions.scalaFunction0ProducingUnitToAction0
+import ImplicitFunctionConversions.schedulerActionToFunc2
+
+/**
+ * Represents an object that schedules units of work.
+ */
+trait Scheduler {
+  def asJava: rx.Scheduler
+
+  /**
+   * Schedules a cancelable action to be executed.
+   *
+   * @param action Action to schedule.
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  def schedule(action: rx.lang.scala.Scheduler => Subscription): Subscription = {
+    this.schedule[Integer](0, (s: Scheduler, x: Integer) => action(s): Subscription): Subscription
+  }
+
+  /**
+   * Schedules a cancelable action to be executed.
+   *
+   * @param state State to pass into the action.
+   * @param action Action to schedule.
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  private def schedule[T](state: T, action: (rx.lang.scala.Scheduler, T) => Subscription): Subscription = {
+    asJava.schedule(state, action)
+  }
+
+  /**
+   * Schedules a cancelable action to be executed in delayTime.
+   *
+   * @param action Action to schedule.
+   * @param delayTime  Time the action is to be delayed before executing.
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  def schedule(delayTime: Duration)(action: Scheduler => Subscription): Subscription = {
+    this.schedule[Integer](0, (s: Scheduler, x: Integer) => action(s), delayTime: Duration): Subscription
+  }
+
+  /**
+   * Schedules a cancelable action to be executed in delayTime.
+   *
+   * @param state
+   *            State to pass into the action.
+   * @param action
+   *            Action to schedule.
+   * @param delayTime
+   *            Time the action is to be delayed before executing.
+   * @param unit
+   *            Time unit of the delay time.
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  private def schedule[T](state: T, action: (Scheduler, T) => Subscription, delayTime: Duration): Subscription = {
+    asJava.schedule(state, action, delayTime.length, delayTime.unit)
+  }
+
+  /**
+   * Schedules a cancelable action to be executed periodically.
+   * This default implementation schedules recursively and waits for actions to complete (instead of potentially executing
+   * long-running actions concurrently). Each scheduler that can do periodic scheduling in a better way should override this.
+   *
+   * @param action The action to execute periodically.
+   * @param initialDelay Time to wait before executing the action for the first time.
+   * @param period The time interval to wait each time in between executing the action.
+   * @return A subscription to be able to unsubscribe from action.
+   */
+  def schedule(initialDelay: Duration, period: Duration)(action: Scheduler => Subscription): Subscription = {
+    this.schedulePeriodically[Integer](0, (s: Scheduler, x:Integer) => action(s): Subscription, initialDelay: Duration, period: Duration): Subscription
+  }
+
+  /**
+   * Schedules a cancelable action to be executed periodically.
+   * This default implementation schedules recursively and waits for actions to complete (instead of potentially executing
+   * long-running actions concurrently). Each scheduler that can do periodic scheduling in a better way should override this.
+   *
+   * @param state
+   *            State to pass into the action.
+   * @param action
+   *            The action to execute periodically.
+   * @param initialDelay
+   *            Time to wait before executing the action for the first time.
+   * @param period
+   *            The time interval to wait each time in between executing the action.
+   * @return A subscription to be able to unsubscribe from action.
+   */
+  private def schedulePeriodically[T](state: T, action: (Scheduler, T) => Subscription, initialDelay: Duration, period: Duration): Subscription = {
+    asJava.schedulePeriodically(state, action, initialDelay.length, initialDelay.unit.convert(period.length, period.unit), initialDelay.unit)
+  }
+
+  /**
+   * Schedules a cancelable action to be executed at dueTime.
+   *
+   * @param action Action to schedule.
+   * @param dueTime Time the action is to be executed. If in the past it will be executed immediately.
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  def schedule(dueTime: Date)(action: Scheduler => Subscription): Subscription = {
+    this.schedule(0: Integer, (s: Scheduler, x: Integer) => action(s): Subscription, dueTime: Date): Subscription
+  }
+
+  /**
+   * Schedules a cancelable action to be executed at dueTime.
+   *
+   * @param state
+   *            State to pass into the action.
+   * @param action
+   *            Action to schedule.
+   * @param dueTime
+   *            Time the action is to be executed. If in the past it will be executed immediately.
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  private def schedule[T](state: T, action: (Scheduler, T) => Subscription, dueTime: Date): Subscription = {
+    asJava.schedule(state, action, dueTime)
+  }
+
+  /**
+   * Schedules an action to be executed.
+   *
+   * @param action
+   *            action
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  def schedule(action: =>Unit): Subscription = {
+    asJava.schedule(()=>action)
+  }
+
+  /**
+   * Schedules an action to be executed in delayTime.
+   *
+   * @param action action
+   * @return a subscription to be able to unsubscribe from action.
+   */
+  def schedule(delayTime: Duration)(action: =>Unit): Subscription = {
+    asJava.schedule(()=>action, delayTime.length, delayTime.unit)
+  }
+
+  /**
+   * Schedules an action to be executed periodically.
+   *
+   * @param action
+   *            The action to execute periodically.
+   * @param initialDelay
+   *            Time to wait before executing the action for the first time.
+   * @param period
+   *            The time interval to wait each time in between executing the action.
+   * @return A subscription to be able to unsubscribe from action.
+   */
+  def schedule(initialDelay: Duration, period: Duration)(action: =>Unit): Subscription = {
+    asJava.schedulePeriodically(()=>action, initialDelay.length, initialDelay.unit.convert(period.length, period.unit), initialDelay.unit)
+  }
+
+  def scheduleRec(work: (=>Unit)=>Unit): Subscription = {
+
+    val subscription = new rx.subscriptions.MultipleAssignmentSubscription()
+
+    subscription.setSubscription(
+      this.schedule(scheduler => {
+        def loop(): Unit =  subscription.setSubscription(scheduler.schedule{ work{ loop() }})
+        loop()
+        subscription
+      }))
+    subscription
+  }
+
+  /**
+   * Returns the scheduler's notion of current absolute time in milliseconds.
+   */
+  def now: Long = {
+    asJava.now
+  }
+
+  /**
+   * Parallelism available to a Scheduler.
+   *
+   * This defaults to {@code Runtime.getRuntime().availableProcessors()} but can be overridden for use cases such as scheduling work on a computer cluster.
+   *
+   * @return the scheduler's available degree of parallelism.
+   */
+  def degreeOfParallelism: Int = {
+    asJava.degreeOfParallelism
+  }
+
+}
+
+/**
+ * Provides constructors for Schedulers.
+ */
+object Scheduler {
+  private class WrapJavaScheduler(val asJava: rx.Scheduler) extends Scheduler
+  
+  /**
+   * Constructs a Scala Scheduler from a Java Scheduler.
+   */
+  def apply(s: rx.Scheduler): Scheduler = new WrapJavaScheduler(s)
+}
