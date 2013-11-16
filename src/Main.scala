01749e19a1e71af76d8d7661ef0dd0348ded8701
headinthebox <emeijer@applied-duality.com>
2013-11-16 12:15:00 -0800

    Added movie example in Scala as Main

--- src/Main.scala
+++ src/Main.scala
@@ -1,16 +1,14 @@
-import java.io.IOException
-import rx.lang.scala.Notification.{OnError, OnCompleted, OnNext}
-import rx.lang.scala.{Notification, Observable}
-import rx.lang.scala.subjects.{ReplaySubject, AsyncSubject}
 
 object Main {
-
   def main(args: Array[String]): Unit = {
-    println("hello")
-    val movies = Observable(new rx.lang.scala.examples.Movie(3000),
-                            new rx.lang.scala.examples.Movie(1000),
-                            new rx.lang.scala.examples.Movie(2000))
-    val lib = new rx.lang.scala.examples.MovieLib(movies)
+    import rx.lang.scala
 
+    val lib = new rx.lang.scala.examples.MovieLib(
+      rx.lang.scala.Observable(new rx.lang.scala.examples.Movie(3000),
+        new rx.lang.scala.examples.Movie(1000),
+        new rx.lang.scala.examples.Movie(2000)))
+
+    lib.longMovies.subscribe(m => println("A movie of length " + m.lengthInSeconds + "s"));
   }
+
 }
