a10c124a7bb72b00c34c681b1dfc4e0102b90ba2
headinthebox <emeijer@applied-duality.com>
2013-11-16 21:38:56 -0800

    Simplified interop example

--- Interop/src/Main.java
+++ Interop/src/Main.java
@@ -1,13 +1,10 @@
 import rx.Observable;
-import rx.lang.scala.Observable$;
 import rx.lang.scala.examples.Movie;
 import rx.lang.scala.examples.MovieLib;
 
-public class Main {
+import static rx.lang.scala.ImplicitFunctionConversions.javaObservableToScalaSObservable;
 
-    public static <T> rx.lang.scala.Observable<T> toScalaObservable(Observable<T> source) {
-        return Observable$.MODULE$.apply(source);
-    }
+public class Main {
 
     public static void main(String[] args) {
 
@@ -17,7 +14,7 @@ public class Main {
                 new Movie(2000)
         );
 
-        MovieLib lib = new MovieLib(toScalaObservable(movies));
+        MovieLib lib = new MovieLib(javaObservableToScalaSObservable(movies));
 
         lib.longMovies().asJavaObservable().subscribe(m ->
                 System.out.println("A movie of length " + m.lengthInSeconds() + "s")
